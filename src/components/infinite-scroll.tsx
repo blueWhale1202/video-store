6e90281fd265800fb292aa147cab4ebbf12f0240
mrfour4 <21521609@gm.uit.edu.vn>
2025-02-21 00:07:30 +0700

    feat: infinite loading

--- src/components/infinite-scroll.tsx
+++ src/components/infinite-scroll.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
+import { Button } from "./ui/button";
 
 type Props = {
     isManual?: boolean;
@@ -17,5 +19,29 @@ export const InfiniteScroll = ({
         threshold: 0.5,
         rootMargin: "100px",
     });
-    return <div></div>;
+
+    useEffect(() => {
+        if (!isManual && inView && hasNextPage && !isFetchingNextPage) {
+            fetchNextPage();
+        }
+    }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage, isManual]);
+
+    return (
+        <div className="flex flex-col items-center gap-4 p-4">
+            <div ref={ref} className="h-1" />
+            {hasNextPage ? (
+                <Button
+                    variant="secondary"
+                    disabled={!hasNextPage || isFetchingNextPage}
+                    onClick={fetchNextPage}
+                >
+                    {isFetchingNextPage ? "Loading..." : "Load More"}
+                </Button>
+            ) : (
+                <p className="text-xs text-muted-foreground">
+                    You have reached the end of the list
+                </p>
+            )}
+        </div>
+    );
 };
--- src/modules/studio/ui/sections/video-section.tsx
+++ src/modules/studio/ui/sections/video-section.tsx
@@ -1,7 +1,17 @@
 "use client";
 
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
+import Link from "next/link";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
@@ -16,7 +26,7 @@ export const VideoSection = () => {
 };
 
 export const VideoSectionSuspense = () => {
-    const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+    const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
         {
             limit: DEFAULT_LIMIT,
         },
@@ -25,6 +35,43 @@ export const VideoSectionSuspense = () => {
         },
     );
     return (
-        <div className="whitespace-pre">{JSON.stringify(data, null, 2)}</div>
+        <div>
+            <div className="border-y">
+                <Table>
+                    <TableCaption>A list of your recent invoices.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[510px] pl-6">
+                                Video
+                            </TableHead>
+                            <TableHead>Visibility</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead className="text-right">Views</TableHead>
+                            <TableHead className="text-right">
+                                Comments
+                            </TableHead>
+                            <TableHead className="pr-6 text-right">
+                                Likes
+                            </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {videos.pages
+                            .flatMap((page) => page.items)
+                            .map((video) => (
+                                <Link
+                                    key={video.id}
+                                    href={`/studio/videos/${video.id}`}
+                                    legacyBehavior
+                                >
+                                    <TableRow className="cursor-pointer">
+                                        <TableCell>{video.title}</TableCell>
+                                    </TableRow>
+                                </Link>
+                            ))}
+                    </TableBody>
+                </Table>
+            </div>
+        </div>
     );
 };
